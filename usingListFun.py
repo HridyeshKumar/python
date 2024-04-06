a08e6d1bd009a6309b23a57b250f7ed951ba763e
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-04-06 06:48:43 +0530

    Add files via upload

---
+++ usingListFun.py
@@ -0,0 +1,17 @@
+#creating an empty list 
+list=[]
+print("Empty List is :",list)
+#creating a list with elements
+my_list= [10,507,"python"] 
+print("Created list is:",my_list)
+#Inserting new elements using append(
+my_list.append(20)
+my_list.append("program")
+my_list.append([3,7])
+print("After deleting elements the new list is:",my_list)
+#deleting elements using pop()
+my_list.pop()
+my_list.pop(2)
+#deleting elements using remove
+my_list.remove(10)
+print("After deleting elements the new list is:",my_list)
