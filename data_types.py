f7760254bce40f873d50b5509bd21913f6852c21
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-04-25 20:07:43 +0530

    Update data_types.py

--- data_types.py
+++ data_types.py
@@ -2,7 +2,7 @@
 a=5
 b=5.7
 c=2+7j
-d="python"
+d="python" 
 print("Value of a:",a,"\t\t\tDatatype is:",type(a))
 print("Value of b:",b,"\t\tDatatype is:",type(b))
 print("Value of c:",c,"\t\tDatatype is:",type(c))
