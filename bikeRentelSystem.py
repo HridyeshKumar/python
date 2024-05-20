8a8b59df3032eb0ef1206d872ad0e2445f11af2b
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-05-20 10:32:25 +0530

    Create bikeRentelSystem.py

---
+++ bikeRentelSystem.py
@@ -0,0 +1,28 @@
+class bikeShop:
+	def _init_(self,stock):
+		self.stock=stock
+	def displayBike(self):
+		print("Total Bikes",self.stock)
+	def rentForBike(self,q):
+				if q<=0:
+					print("Enter the positive value or greater than zero")
+				elif q>self.stock:
+					print("Enter the value(less than stock)")
+				else:
+						self.stock=self.stock-q
+						print("Total Prices",q*100)
+						print("Total Bikes",self.stock)
+while True:
+	obj=bikeShop(100)
+	uc=int(input('''
+1 Display Stocks
+2 Rent A Bike
+3 Exit
+				'''))
+	if uc==1:
+		obj.displayBike()
+	elif uc==2:
+		n=int(input("Enter The Quantity:"))
+		obj.rentForBike(n)
+	else:
+		break
