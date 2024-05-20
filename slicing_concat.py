98f3d00d801c392a97c1a110b33d36604fe9997e
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-05-20 10:19:37 +0530

    Add files via upload

---
+++ slicing_concat.py
@@ -0,0 +1,14 @@
+import pandas as pd # type: ignore
+
+# Initializing the nested list with Data set
+player_list = [['M.S.Dhoni', 36, 75, 5428000],
+			['A.B.D Villers', 38, 74, 3428000],
+			['V.Kohli', 31, 70, 8428000],
+			['S.Smith', 34, 80, 4428000],
+			['C.Gayle', 40, 100, 4528000],
+			['J.Root', 33, 72, 7028000],
+			['K.Peterson', 42, 85, 2528000]]
+
+# creating a pandas dataframe
+df = pd.DataFrame(player_list, columns=['Name', 'Age', 'Weight', 'Salary'])
+df # data frame before slicing
