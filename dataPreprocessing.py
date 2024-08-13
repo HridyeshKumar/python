c8fccd027363e882f26d4488382e1f651c2cde2b
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-08-13 09:09:39 +0530

    Update dataPreprocessing.py

--- dataPreprocessing.py
+++ dataPreprocessing.py
@@ -56,3 +56,85 @@ titanic_data.head()
 titanic_data=titanic_data[["survived","pclass","age","fare"]]
 titanic_data.head()
 titanic_data.isnull().mean()
+titanic_data.isnull().sum()
+median=titanic_data.age.median()
+print(median)
+mean=titanic_data.age.mean()
+print(mean)
+titanic_data["Median_Age"]=titanic_data.age.fillna(median)
+titanic_data["Mean_Age"]=titanic_data.age.fillna(mean)
+titanic_data["Mean_Age"]=np.round(titanic_data["Mean_Age"],1)
+titanic_data.head(20)
+
+#Frequent Category Imputation
+'''One of the most common ways of handling missing values in a categorial column is to replace the missing values with the most frequent occuring values i.e., the mode of the column.'''
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+titanic_data=sns.load_dataset("titanic")
+titanic_data=titanic_data[["embark_town","age","fare"]]
+titanic_data.head()
+titanic_data.isnull().mean()
+titanic_data.embark_town.value_counts().sort_values(ascending=False).plot.bar()
+plt.xlabel("Embark Town")
+plt.ylabel("Number of Passengers")
+titanic_data.embark_town.mode()
+titanic_data.embark_town.fillna("Southampton",inplace=True)
+
+#Categorial Data Encoding
+'''Models based on statistical algorithms such as machine learning and deep learning work with numbers.
+A dataset can contain numerical, categorical, datetime, and mixed variables.
+A mechanism is needed to convert categorical data to its numeric counterpart so that the data can be used to build statistical models.
+The techniques used to convert numeric data into categorical data are called categorical data encoding schemes.'''
+
+#One Hot Encoding
+'''One Hot Encoding is one of the most commonly used categorical encoding schemes.
+In one hot encoding for each unique value in the categorical column a new column is added.
+Integer 1 is added to the column that corresponds to the original label and all the remaining column are filled with zeros.'''
+
+titanic_data=sns.load_dataset("titanic")
+titanic_data.head()
+titanic_data=titanic_data[["sex","class","embark_town"]]
+titanic_data.head()
+
+import pandas as pd 
+temp=pd.get_dummies(titanic_data["sex"])
+temp.head()
+pd.concat([titanic_data["sex"],pd.get_dummies(titanic_data["sex"])],axis=1).head()
+temp=pd.get_dummies(titanic_data["embark_town"])
+temp.head()
+
+#Label Encoding
+'''In label encoding, labels are replaced by integers.
+That is why label encoding is also called as Integer Encoding.'''
+
+from sklearn.preprocessing import LabelEncoder
+le=LabelEncoder()
+le.fit(titanic_data["class"])
+titanic_data["le_class"]=le.transform(transform(titanic_data["class"])
+titanic_data.head()
+
+#Data Discretization
+'''The process of converting continuous numeric values such as price, age, and weight into discrete intervals is called discretization or binning.'''
+
+#Equal Width Discretization
+'''The most common type of discretization approach is fixed width discretization.'''
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+
+import warnings
+warnings.filterwarnings("ignore")
+diamond_data=sns.load_dataset("diamonds")
+diamonds_data.head()
+sns.distplot(diamond_data["price"])
+
+'''The histogram for price column shows that the data is positively skewed.'''
+
+price_range=diamond_data["price"].max()-diamond_data["price"].min()
+print(price_range)
+price_range/10
+
