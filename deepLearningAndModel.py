5e343baa9b71f7e8ec2f1c872d7d8cd74abbae4d
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-08-17 10:55:32 +0530

    Update

---
+++ deepLearningAndModel.py
@@ -0,0 +1,9 @@
+#Introduction to Deep Learning and Model on Iris Dataset
+#Deep Learning Framework
+'''Deep Learning is a field within Machine Learning that deals with building amd using Neural Network Models.
+Neural Networks mimic the functioning of a human brain.
+Neural Networks with more than three layers are typically categorised as Deep Learning Networks.'''
+#Perceptron
+'''The Perceptron is the unit of learning in an artificial neural networks.A Perceptron resembles a human brain cell.
+A Perceptron is a single cell or node in a neural network.
+ '''
