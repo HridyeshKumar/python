cf02efdbf10a8efdae8c52601a725b2bc7315c63
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-08-19 12:57:50 +0530

    Update

---
+++ project5onRecommendation.py
@@ -0,0 +1,4 @@
+#Movie Recommender System
+'''Recommender Systems, also labelled as recommendation systems, are statistical algorithms that recommend products to users based on similarities between the buying trends of various user or similarities between the products.
+
+Collaborative Filtering:- The process used to calculate similaritiies between the buying trends of various users or similarities between products is called collaborative filtering.'''
