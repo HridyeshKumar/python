0bd3689a4c594d6703c8d9c35f1ca3b29c0971cc
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-08-22 16:22:27 +0530

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,4 +1,4 @@
-name = "Hariram is a good  boy and  "
+name = "Hariram is a good  boy and  Geeta is a good girl."
 
 print(name.replace("  ", " "))
 print(name) # Strings are immutable which means that you cannot change them by running functions on them
