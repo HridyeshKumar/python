5ac66cd14c29095487429d86145f5dbe775d9493
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-08-22 16:42:40 +0530

    Create constructor.py

---
+++ constructor.py
@@ -0,0 +1,24 @@
+class Employee: 
+    language = "Python" # This is a class attribute
+    salary = 1200000
+
+    def __init__(self, name, salary, language): # dunder method which is automatically called
+        self.name = name
+        self.salary = salary
+        self.language = language
+        print("I am creating an object")
+ 
+ 
+    def getInfo(self):
+        print(f"The language is {self.language}. The salary is {self.salary}")
+
+    @staticmethod
+    def greet():
+        print("Good morning")
+
+
+harsh = Employee("Harsh", 1300000, "JavaScript") 
+# harsh.name = "Harsh"
+print(harsh.name, harsh.salary, harsh.language)
+
+rohan = Employee()
