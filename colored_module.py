ceece1408946f9de31795db5c5e426bf6dd6757a
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-09-24 23:41:57 +0530

    Create colored_module.py

---
+++ colored_module.py
@@ -0,0 +1,2 @@
+from termcolor import colored
+print(colored("H","red"),colored("e","yellow"),colored("l","green"),colored("l","cyan"),colored("o","blue"),colored(",","magenta"),colored("w","red"),colored("o","yellow"),colored("r","green"),colored("l","cyan"),colored("d","blue"))
